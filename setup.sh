2ff37a2875e4bdb7d727ae9fb51881e1467baf2b
jam <jameshikelly@gmail.com>
2025-03-03 15:28:46 -0500

    readded my changes after accidently deleting

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 
 # assuming jam-configfiles are home (~/)  
 ln -sf ~/jam-configfiles/.vimrc ~/.vimrc
-ln -sf ~/jam-configfiles/.bashrc ~/bashrc
+ln -sf ~/jam-configfiles/.bashrc ~/.bashrc
 ln -sf ~/jam-configfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/jam-configfiles/.config ~/.config
 ln -sf ~/jam-configfiles/.vim ~/.vim
-
+ln -sf ~/jam-configfiles/.git-prompt.sh ~/.git-prompt.sh
 
 

d5f14e075351b837aa75d8293832c009d78aea33
jamtholomew <jameshikelly@gmail.com>
2025-04-14 11:40:10 -0400

    Added verbose to setup.sh script

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 git submodule update --init --recursive
 
 # symbolic linking w/ curr directory 
-pwd | xargs -I {} ln -sf {}/.vimrc ~/.vimrc
-pwd | xargs -I {} ln -sf {}/.bashrc ~/.bashrc
-pwd | xargs -I {} ln -sf {}/.tmux.conf ~/.tmux.conf
-pwd | xargs -I {} ln -sf {}/.config ~/.config
-pwd | xargs -I {} ln -sf {}/.vim ~/.vim
-pwd | xargs -I {} ln -sf {}/.git-prompt.sh ~/.git-prompt.sh
+pwd | xargs -I {} ln -sfv {}/.vimrc ~/.vimrc
+pwd | xargs -I {} ln -sfv {}/.bashrc ~/.bashrc
+pwd | xargs -I {} ln -sfv {}/.tmux.conf ~/.tmux.conf
+pwd | xargs -I {} ln -sfv {}/.config ~/.config
+pwd | xargs -I {} ln -sfv {}/.vim ~/.vim
+pwd | xargs -I {} ln -sfv {}/.git-prompt.sh ~/.git-prompt.sh
 
-
+source ~/.bashrc 
